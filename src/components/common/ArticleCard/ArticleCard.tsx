b5e3d22c216f5b56d5d234ad6dc3ca8f0c48b4d0
vhs24 <162656396+trungquoc01@users.noreply.github.com>
2024-03-19 11:31:46 +0700

    fix create at

--- src/components/common/ArticleCard/ArticleCard.tsx
+++ src/components/common/ArticleCard/ArticleCard.tsx
@@ -18,6 +18,8 @@ import {
 } from '@ant-design/icons';
 import dbService from '@app/pages/DashBoard/DashBoardService';
 import Meta from 'antd/lib/card/Meta';
+import moment from 'moment';
+import 'moment/locale/vi';
 interface ArticleCardProps {
   idPost: number;
   author?: React.ReactNode;
@@ -136,7 +138,7 @@ export const ArticleCard: React.FC<ArticleCardProps> = ({
             </S.UserName>
           </S.InfoAvt>
           <S.InfoHeader>
-            <S.Description>{date}</S.Description>
+            <S.Description>{moment(new Date(date)).locale('vi').format('lll')}</S.Description>
           </S.InfoHeader>
         </S.Header>
         <S.InfoWrapper>
@@ -199,7 +201,11 @@ export const ArticleCard: React.FC<ArticleCardProps> = ({
           <S.WrapperOnloadCmt className={className}>
             <S.Header>
               <S.InfoAvt>
-                <Avatar src={avatar ? `http://localhost:8081/local-store/${avatar}` : dfavt} alt="author" size={43} />{' '}
+                <Avatar
+                  src={avatar ? `http://localhost:8081/local-store/${avatar}` : dfavt}
+                  alt="author"
+                  size={43}
+                />{' '}
                 <S.UserName>
                   {author} {isExpert ? <CheckCircleTwoTone /> : null}
                 </S.UserName>
@@ -242,7 +248,14 @@ export const ArticleCard: React.FC<ArticleCardProps> = ({
                 >
                   <Meta
                     avatar={<Avatar src={`http://localhost:8081/local-store/${item.userId.imageUrl}`} />}
-                    title={item.userId.name}
+                    title={
+                      <>
+                        {item.userId.name}
+                        <p style={{ marginRight: '5px', fontSize: '0.75rem' }}>
+                          {moment(new Date(item.createAt)).locale('vi').format('lll')}
+                        </p>
+                      </>
+                    }
                   />
                   {item.comemntParent && (
                     <S.CardCmt
@@ -255,13 +268,23 @@ export const ArticleCard: React.FC<ArticleCardProps> = ({
                       }}
                       bodyStyle={{ display: 'flex', flexDirection: 'column', alignItems: 'flex-start' }}
                     >
-                      <Meta style={{ fontSize: '0.75rem' }} title={item.comemntParent.userId.name} />
-                      <p style={{ marginTop: '2%', fontSize: '0.9rem', marginBottom: '0em' }}>
+                      <Meta
+                        style={{ fontSize: '0.75rem' }}
+                        title={
+                          <>
+                            {item.comemntParent.userId.name}
+                            <p style={{ marginRight: '5px', fontSize: '0.75rem' }}>
+                              {moment(new Date(item.comemntParent.createAt)).locale('vi').format('lll')}
+                            </p>
+                          </>
+                        }
+                      />
+                      <p style={{ marginTop: '2%', fontSize: '1rem', marginBottom: '0em' }}>
                         {item.comemntParent.content}
                       </p>
                     </S.CardCmt>
                   )}
-                  <p style={{ marginTop: '2%', marginLeft: '10%', fontSize: '0.9rem' }}>{item.content}</p>
+                  <p style={{ marginTop: '2%', marginLeft: '10%', fontSize: '1rem' }}>{item.content}</p>
                   {reply === item.id ? null : (
                     <Button
                       style={{ marginLeft: '10%' }}
