1c9ada4d010e72f2469562f72662cca283d32eeb
vhs24 <162656396+trungquoc01@users.noreply.github.com>
2024-03-20 10:03:03 +0700

    don profile and navigate

--- src/components/common/ArticleCard/ArticleCard.tsx
+++ src/components/common/ArticleCard/ArticleCard.tsx
@@ -18,6 +18,8 @@ import {
 } from '@ant-design/icons';
 import dbService from '@app/pages/DashBoard/DashBoardService';
 import Meta from 'antd/lib/card/Meta';
+import moment from 'moment';
+import 'moment/locale/vi';
 interface ArticleCardProps {
   idPost: number;
   author?: React.ReactNode;
@@ -136,7 +138,7 @@ export const ArticleCard: React.FC<ArticleCardProps> = ({
             </S.UserName>
           </S.InfoAvt>
           <S.InfoHeader>
-            <S.Description>{date}</S.Description>
+            <S.Description>{moment(new Date(date)).locale('vi').format('lll')}</S.Description>
           </S.InfoHeader>
         </S.Header>
         <S.InfoWrapper>
@@ -224,7 +226,7 @@ export const ArticleCard: React.FC<ArticleCardProps> = ({
               <S.Hashtag>#{hashTags}</S.Hashtag>
             </S.InfoWrapper>
 
-            <S.ImageWrap>
+            <S.ImageWrap2>
               {imgUrl?.map((img: string) => (
                 <Image
                   src={`http://localhost:8081/local-store/${img}`}
@@ -234,7 +236,7 @@ export const ArticleCard: React.FC<ArticleCardProps> = ({
                   style={{ objectFit: 'contain', width: '90%' }}
                 />
               ))}
-            </S.ImageWrap>
+            </S.ImageWrap2>
             {comments.map((item: any) => {
               return (
                 <S.CardCmt
@@ -247,7 +249,14 @@ export const ArticleCard: React.FC<ArticleCardProps> = ({
                 >
                   <Meta
                     avatar={<Avatar src={`http://localhost:8081/local-store/${item.userId.imageUrl}`} />}
-                    title={item.userId.name}
+                    title={
+                      <>
+                        {item.userId.name}
+                        <p style={{ marginRight: '5px', fontSize: '0.75rem' }}>
+                          {moment(new Date(item.createAt)).locale('vi').format('lll')}
+                        </p>
+                      </>
+                    }
                   />
                   {item.comemntParent && (
                     <S.CardCmt
@@ -260,13 +269,23 @@ export const ArticleCard: React.FC<ArticleCardProps> = ({
                       }}
                       bodyStyle={{ display: 'flex', flexDirection: 'column', alignItems: 'flex-start' }}
                     >
-                      <Meta style={{ fontSize: '0.75rem' }} title={item.comemntParent.userId.name} />
-                      <p style={{ marginTop: '2%', fontSize: '0.9rem', marginBottom: '0em' }}>
+                      <Meta
+                        style={{ fontSize: '0.75rem' }}
+                        title={
+                          <>
+                            {item.comemntParent.userId.name}
+                            <p style={{ marginRight: '5px', fontSize: '0.75rem' }}>
+                              {moment(new Date(item.comemntParent.createAt)).locale('vi').format('lll')}
+                            </p>
+                          </>
+                        }
+                      />
+                      <p style={{ marginTop: '2%', fontSize: '1rem', marginBottom: '0em' }}>
                         {item.comemntParent.content}
                       </p>
                     </S.CardCmt>
                   )}
-                  <p style={{ marginTop: '2%', marginLeft: '10%', fontSize: '0.9rem' }}>{item.content}</p>
+                  <p style={{ marginTop: '2%', marginLeft: '10%', fontSize: '1rem' }}>{item.content}</p>
                   {reply === item.id ? null : (
                     <Button
                       style={{ marginLeft: '10%' }}
@@ -289,7 +308,7 @@ export const ArticleCard: React.FC<ArticleCardProps> = ({
             })}
           </S.WrapperOnloadCmt>
           <S.WrapperCmt>
-            <Input onChange={(event) => setComment(event.target.value)} />
+            <Input value={comment} onChange={(event) => setComment(event.target.value)} />
             <Button onClick={() => UpComment()}>
               <SendOutlined />
             </Button>
