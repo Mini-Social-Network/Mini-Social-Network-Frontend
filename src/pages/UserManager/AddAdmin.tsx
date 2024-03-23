bf4c08a2a7a9a385c3102ef3d4346f2abad22f33
trungquoc01 <162656396+trungquoc01@users.noreply.github.com>
2024-03-23 15:06:05 +0700

    fix ui

--- src/pages/UserManager/AddAdmin.tsx
+++ src/pages/UserManager/AddAdmin.tsx
@@ -31,7 +31,6 @@ export const AddAdmin: React.FC<DBProps> = ({ onAddSuccess }) => {
   const [isLoading, setLoading] = useState(false);
   const [fileList, setFileList] = useState<UploadFile[]>([]);
 
-
   const [name, setName] = useState(null);
   const [email, setEmail] = useState(null);
   const [password, setPassword] = useState(null);
@@ -43,7 +42,6 @@ export const AddAdmin: React.FC<DBProps> = ({ onAddSuccess }) => {
   const handleUpload = async () => {
     setLoading(true);
 
-    let idCardBase64 = '';
     if (fileList.length > 0) {
       await getBase64(fileList, (result: string) => {
         const formData = {
@@ -115,7 +113,7 @@ export const AddAdmin: React.FC<DBProps> = ({ onAddSuccess }) => {
     fileList,
   };
   return (
-    <BaseForm FF>
+    <BaseForm>
       <BaseForm.Item name="name" label="Tên" required>
         <Input onChange={(event: any) => setName(event.target.value)} required />
       </BaseForm.Item>
